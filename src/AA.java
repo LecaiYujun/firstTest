2098584b184931af2396fbc2d00d7e0caef15d52
yujun <158124925@qq.com>
2018-12-18 15:26:11 +0800

    Two

--- src/AA.java
+++ src/AA.java
@@ -2,5 +2,6 @@ public class AA {
     public static void main(String[] args) {
         String a="sss";
         System.out.println("AAAAAAAA"+a);
+        System.out.println("BBBBBBBBBBBBBB");
     }
 }
